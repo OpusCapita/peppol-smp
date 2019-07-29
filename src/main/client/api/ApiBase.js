8f024d9bc7b352ab1137aeacbc6ed3e4c9a5bb95
thebilge <ibrahimbilge7@gmail.com>
2019-07-29 17:31:16 +0200

    implemented lookup logic

--- src/main/client/api/ApiBase.js
+++ src/main/client/api/ApiBase.js
@@ -5,8 +5,8 @@ class ApiBase {
 
     ajax = request;
 
-    getTransmissionList(pagination, filter) {
-        return this.ajax.post(`/peppol-monitor/api/get-transmissions`).send({pagination, filter}).then(res => res.body).catch(ApiError.getErrorFromResponse);
+    getLookup(icd, identifier) {
+        return this.ajax.get(`/peppol-smp/api/lookup/${icd}/${identifier}`).then(res => res.body).catch(ApiError.getErrorFromResponse);
     }
 }
 
--- src/main/client/components/PeppolSmp/PeppolSmp.react.js
+++ src/main/client/components/PeppolSmp/PeppolSmp.react.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import {ApiBase} from '../../api';
 import {Components} from '@opuscapita/service-base-ui';
 import './PeppolSmp.css';
 
 class PeppolSmp extends Components.ContextComponent {
 
-    state = {};
+    state = {
+        loading: false,
+        icd: '',
+        identifier: '',
+        result: {participants: []}
+    };
 
     constructor(props, context) {
         super(props);
+
+        this.api = new ApiBase();
     }
 
     componentDidMount() {
@@ -23,71 +31,111 @@ class PeppolSmp extends Components.ContextComponent {
         this.context.router.push(`/peppol-smp/${page}`);
     }
 
+    handleFormChange(field, value) {
+        const searchValues = {};
+        searchValues[field] = value;
+        this.setState(searchValues);
+    }
+
+    lookup() {
+        if (!this.state.icd || !this.state.identifier) {
+            this.context.showNotification('Enter icd number and participant identifier first.', 'info', 3);
+            return;
+        }
+
+        this.setState({loading: true});
+        this.api.getLookup(this.state.icd, this.state.identifier).then((response) => {
+            this.setState({loading: false, result: response});
+        }).catch(e => {
+            this.setState({loading: false});
+            this.context.showNotification(e.message, 'error', 10);
+        });
+    }
+
     render() {
-        // noinspection HtmlUnknownTarget
+        const {loading, icd, identifier, result} = this.state;
         return (
             <div>
                 <h2>PEPPOL Participant Management</h2>
                 <div className="form-horizontal smp-home">
                     <div className="flex-box">
                         <div className="flex-item-5">
-                            <input type="text" placeholder="ISO 6523 identifier" className="form-control"/>
+                            <input type="text" placeholder="ISO 6523 identifier" className="form-control"
+                                   value={icd} onChange={e => this.handleFormChange('icd', e.target.value)}/>
                         </div>
                         <div className="flex-item-5">
-                            <input type="text" placeholder="Participant identifier" className="form-control"/>
+                            <input type="text" placeholder="Participant identifier" className="form-control"
+                                   value={identifier}
+                                   onChange={e => this.handleFormChange('identifier', e.target.value)}/>
                         </div>
                         <div className="flex-item-2">
-                            <button className="btn btn-primary">Search</button>
+                            <button className="btn btn-primary" onClick={() => this.lookup()}>Search</button>
                         </div>
                     </div>
-                    <h3 className="lookup-result-header"><span>9908:21356233</span></h3>
-                    <div className="lookup-result row">
-                        <div className="col-md-12">
-                            <div className="form-group">
-                                <div className="col-sm-4">
-                                    <label className="control-label btn-link">Access Point</label>
-                                </div>
-                                <div className="offset-md-1 col-sm-7">
-                                    <label className="control-label">OpusCapita Peppol Production Access Point</label>
-                                </div>
+                    {
+                        result.participants.length &&
+                        this.renderParticipants()
+                    }
+                </div>
+            </div>
+        );
+    }
+
+    renderParticipants() {
+        const list = [];
+
+        this.state.result.participants.forEach((p) => {
+            list.push(<div>
+                <h3 className="lookup-result-header"><span>{p.icd}:{p.identifier}</span></h3>
+                <div className="lookup-result row">
+                    <div className="col-md-12">
+                        <div className="form-group">
+                            <div className="col-sm-3">
+                                <label className="control-label btn-link">Access Point</label>
                             </div>
-                            <div className="form-group">
-                                <div className="col-sm-4">
-                                    <label className="control-label btn-link">Transport Protocol</label>
-                                </div>
-                                <div className="offset-md-1 col-sm-7">
-                                    <label className="control-label">busdox-transport-as2-ver1p0</label>
-                                </div>
+                            <div className="col-sm-9">
+                                <label className="control-label">{p.apDescription}</label>
                             </div>
-                            <div className="form-group">
-                                <div className="col-sm-4">
-                                    <label className="control-label btn-link">Access Point URL</label>
-                                </div>
-                                <div className="offset-md-1 col-sm-7">
-                                    <label className="control-label">https://peppol.itella.net/peppol-ap-inbound/as2</label>
-                                </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-3">
+                                <label className="control-label btn-link">Transport Protocol</label>
                             </div>
-                            <div className="form-group">
-                                <div className="col-sm-4">
-                                    <label className="control-label btn-link">Access Point Certificate</label>
-                                </div>
-                                <div className="offset-md-1 col-sm-7">
-                                    <label className="control-label">C=FI, O=OpusCapita Solutions Oy, OU=PEPPOL PRODUCTION AP, CN=PNO000104</label>
-                                </div>
+                            <div className="col-sm-9">
+                                <label className="control-label">{p.apProtocol}</label>
                             </div>
-                            <div className="form-group">
-                                <div className="col-sm-4">
-                                    <label className="control-label btn-link">Contact</label>
-                                </div>
-                                <div className="offset-md-1 col-sm-7">
-                                    <label className="control-label">opuscapita.com</label>
-                                </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-3">
+                                <label className="control-label btn-link">Access Point URL</label>
+                            </div>
+                            <div className="col-sm-9">
+                                <label
+                                    className="control-label">{p.apUrl}</label>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-3">
+                                <label className="control-label btn-link">Access Point Certificate</label>
+                            </div>
+                            <div className="col-sm-9">
+                                <label className="control-label">{p.apCertificate}</label>
+                            </div>
+                        </div>
+                        <div className="form-group">
+                            <div className="col-sm-3">
+                                <label className="control-label btn-link">Contact</label>
+                            </div>
+                            <div className="col-sm-9">
+                                <label className="control-label">{p.contact}</label>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            </div>);
+        });
+
+        return list;
     }
 }
 
