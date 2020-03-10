2d8681be2426140c4cf8ad3358791922727718cd
thebilge <ibrahimbilge7@gmail.com>
2020-03-10 15:04:01 +0100

    adding health check

--- src/main/java/com/opuscapita/peppol/smp/controller/SmpHomeController.java
+++ src/main/java/com/opuscapita/peppol/smp/controller/SmpHomeController.java
@@ -1,5 +1,6 @@
 package com.opuscapita.peppol.smp.controller;
 
+import com.opuscapita.peppol.smp.controller.dto.CommonRestResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,5 +10,10 @@ public class SmpHomeController {
     @RequestMapping(value = "/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/api/health/check")
+    public CommonRestResponse health() {
+        return new CommonRestResponse("Yes, I'm alive!");
     }
 }
---
+++ src/main/java/com/opuscapita/peppol/smp/controller/dto/CommonRestResponse.java
@@ -0,0 +1,14 @@
+package com.opuscapita.peppol.smp.controller.dto;
+
+public class CommonRestResponse {
+
+    private String message;
+
+    public CommonRestResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
