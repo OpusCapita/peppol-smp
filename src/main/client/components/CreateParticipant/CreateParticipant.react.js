2693d690c948783945ec6088c0275cab0d3067e9
thebilge <ibrahimbilge7@gmail.com>
2020-03-29 18:50:18 +0200

    fixed client imports

--- src/main/client/components/CreateParticipant/CreateParticipant.react.js
+++ src/main/client/components/CreateParticipant/CreateParticipant.react.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import {ApiBase, Countries, IcdValues} from '../../api';
 import {Components} from '@opuscapita/service-base-ui';
 import Select from '@opuscapita/react-select';
